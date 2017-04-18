2c24010529ef41911b01a9185ebcf343fd77666b
Mathias Iden <mathiasiden@me.com>
2017-04-18 13:10:47 +0100

    Finished

--- indexer.py
+++ indexer.py
@@ -4,6 +4,9 @@ from whoosh.index import create_in, open_dir
 from whoosh.query import *
 from whoosh.qparser import QueryParser
 from whoosh import scoring
+from whoosh import qparser
+
+# Please see the main() function that utilizes all the helper functions to print p@10
 
 # Set up index and schema
 if not os.path.exists("index"):
@@ -12,7 +15,7 @@ schema = Schema(title=TEXT(stored=True), content=TEXT)
 ix = create_in("index", schema)
 ix = open_dir("index")
 
-def openAndParse(path):
+def _openAndParse(path):
     rawDocs = open(path, "r")
     formattedDocs = []
     title = None
@@ -28,8 +31,8 @@ def openAndParse(path):
             content += strippedLine
     return formattedDocs
 
-def addParsedDocsToIndex():
-    parsedDocs = openAndParse("./med/MED.ALL")
+def _addParsedDocsToIndex():
+    parsedDocs = _openAndParse("./med/MED.ALL")
     writer = ix.writer()
     for document in parsedDocs:
         title = document[0]
@@ -37,16 +40,16 @@ def addParsedDocsToIndex():
         writer.add_document(title=title, content=content)
     writer.commit()
 
-def runQuery(querystring):
+def _runQuery(querystring):
     savedResults = []
     with ix.searcher(weighting=scoring.TF_IDF()) as searcher:
-        query = QueryParser('content', ix.schema).parse(querystring)
+        query = QueryParser('content', ix.schema, group=qparser.OrGroup).parse(querystring)
         results = searcher.search(query)
         for result in results:
             savedResults.append(result.get("title"))
         return savedResults
 
-def getRelevantDocList():
+def _getRelevantDocList():
     queryIdWithRelevantDocs = {}
     with open("./med/MED.REL") as relDocs:
         for line in relDocs:
@@ -60,18 +63,34 @@ def getRelevantDocList():
         return queryIdWithRelevantDocs
 
 
-def getPrecisionAtTen(queries, queryIdWithRelevantDocs):
+def _getPrecisionAtTen(queries, queryIdWithRelevantDocs):
     pAtTen = {}
     for query in queries:
         queryId = query[0]
         content = query[1]
-        results = runQuery(content)
-        pAtTen[queryId] = len(results)
+        results = _runQuery(content)
+        knownRelevantResults = queryIdWithRelevantDocs[queryId]
+        relevantDocsInTopTenResults = 0
+        for result in results:
+            if result in knownRelevantResults:
+                relevantDocsInTopTenResults += 1
+        pAtTen[queryId] = relevantDocsInTopTenResults / 10.0
     return pAtTen
 
-addParsedDocsToIndex()
-queryIdWithRelevantDocs = getRelevantDocList()
-queries = openAndParse("./med/MED.QRY")
-precisionsAtTen = getPrecisionAtTen(queries, queryIdWithRelevantDocs)
-for p in precisionsAtTen.values():
-    print(p)
+# This function does the following:
+# 1. Index the docs from MED.ALL
+# 2. queryIdWithRelevantDocs: Makes a dictionary with queryId as key and array of documentIds
+# 3. queries: Get a list of queries along with the query id
+# 4. precisionAtTen: Queries the index with queries from MED.QRY and checks how many of the first ten docs are relevant
+def main():
+    _addParsedDocsToIndex()
+    queryIdWithRelevantDocs = _getRelevantDocList()
+    queries = _openAndParse("./med/MED.QRY")
+    precisionsAtTen = _getPrecisionAtTen(queries, queryIdWithRelevantDocs)
+
+    print("=============")
+    print("Query id | P@10 ")
+    for queryId, pAtTen in precisionsAtTen.items():
+        print(str(queryId) + ": " + str(pAtTen))
+
+main()
